f1f650b57f7717f0ab48709543d4699b6093e924
laila-93 <lailazai3@outlook.com>
2025-02-05 09:30:47 +0100

    Update main.c

--- ovning/ovning_3/main/main.c
+++ ovning/ovning_3/main/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freetors/task.h"
+#include "driver/gpio.h"
+
+
+#define BLINK_GPIO GPIO_NUM_2
+
 
 void app_main(void)
 {
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1){
+        
+    }
+
 
 }
