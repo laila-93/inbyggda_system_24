cb5af0dc80f9eb99a793a4736a96653322571d84
laila-93 <lailazai3@outlook.com>
2025-02-07 10:36:42 +0100

    summary of ovning_5

--- ovning/ovning_5/main/main.c
+++ ovning/ovning_5/main/main.c
@@ -7,28 +7,32 @@
 void app_main(void)
 { 
     const uart_port_t uart_num = UART_NUM_1;
-uart_config_t uart_config = {
+    uart_config_t uart_config = {
     .baud_rate = 115200,
     .data_bits = UART_DATA_8_BITS,
     .parity = UART_PARITY_DISABLE,
     .stop_bits = UART_STOP_BITS_1,
-    .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,  
     .rx_flow_ctrl_thresh = 122,
-};
-// Configure UART parameters
-ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
 
+    };
+    // Configure UART parameters
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
 
-
-    esp_err_t uart_driver_install(uart_port_t uart_num, int rx_buffer_size, int tx_buffer_size, 
-    int queue_size, QueueHandle_t *uart_queue, int intr_alloc_flags);
 
     do{
          // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
-ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 3, 5, 16, 18)); 
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 2, 3, 16, 18)); 
 
-    }
+    // Setup UART buffered IO with event queue
+    const int uart_buffer_size = (1024 * 2);
+    QueueHandle_t uart_queue;
+    
+    // Install UART driver using an event queue here
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, uart_buffer_size,
+                                            uart_buffer_size, 10, &uart_queue, 0));
+
+    } while (1); 
 
 
-
-} 
+}
